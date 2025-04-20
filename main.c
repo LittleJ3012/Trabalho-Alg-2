a667bfbf52058ade79c6b0a6654c1512ebe289c7
Julia Barcellos Paiva <105679238+LittleJ3012@users.noreply.github.com>
2025-04-20 15:22:18 -0300

    Update main.c

--- main.c
+++ main.c
@@ -43,84 +43,119 @@ int main(void) {
         vetor = geraOrdenados(tam_vetor, ordem);
     }
 
-    printf("\nVetor criado: \n");
-    imprimeVet(vetor, tam_vetor);
-
-    vetorCopia = copiaVet(vetor, tam_vetor);
-    printf("ORDENACAO POR BOLHA:\n");
-    mede_tempo_execucao_quadratico(bolha, vetorCopia, tam_vetor, vet_info);
-    imprimeVet(vetorCopia, tam_vetor);
-    printf("BOLHA realizou %lld trocas.\n", vet_info[0]);
-    printf("BOLHA realizou %lld comparacoes.\n", vet_info[1]);
-    printf("BOLHA realizou %lld chamadas de funcao.\n", vet_info[2]);
-    printf("Tempo de execucao do BOLHA: %lld microssegundos\n\n", vet_info[3]);
-    salvar_resultado("Bolha", vet_info, tam_vetor);
-
-    vetorCopia = copiaVet(vetor, tam_vetor);
-    reseta_vet_info(vet_info);
-    printf("ORDENACAO POR BOLHA INTELIGENTE:\n");
-    mede_tempo_execucao_quadratico(bolhaInteligente, vetorCopia, tam_vetor, vet_info);
-    imprimeVet(vetorCopia, tam_vetor);
-    printf("BOLHA INTELIGENTE realizou %lld trocas.\n", vet_info[0]);
-    printf("BOLHA INTELIGENTE realizou %lld comparacoes.\n", vet_info[1]);
-    printf("BOLHA INTELIGENTE realizou %lld chamadas de funcao.\n", vet_info[2]);
-    printf("Tempo de execucao do BOLHA INTELIGENTE: %lld microssegundos\n\n", vet_info[3]);
-    salvar_resultado("Bolha Inteligente", vet_info, tam_vetor);
-
-    vetorCopia = copiaVet(vetor, tam_vetor);
-    reseta_vet_info(vet_info);
-    printf("ORDENACAO POR SELECAO:\n");
-    mede_tempo_execucao_quadratico(selecao, vetorCopia, tam_vetor, vet_info);
-    imprimeVet(vetorCopia, tam_vetor);
-    printf("SELECAO realizou %lld trocas.\n", vet_info[0]);
-    printf("SELECAO realizou %lld comparacoes.\n", vet_info[1]);
-    printf("SELECAO realizou %lld chamadas de funcao.\n", vet_info[2]);
-    printf("Tempo de execucao do SELEÇÃO: %lld microssegundos\n\n", vet_info[3]);
-    salvar_resultado("Selecao", vet_info, tam_vetor);
-
-    vetorCopia = copiaVet(vetor, tam_vetor);
-    reseta_vet_info(vet_info);
-    printf("ORDENACAO POR INSERCAO:\n");
-    mede_tempo_execucao_quadratico(insercao, vetorCopia, tam_vetor, vet_info);
-    imprimeVet(vetorCopia, tam_vetor);
-    printf("INSERCAO realizou %lld movimentacoes.\n", vet_info[0]);
-    printf("INSERCAO realizou %lld comparacoes.\n", vet_info[1]);
-    printf("INSERCAO realizou %lld chamadas de funcao.\n", vet_info[2]);
-    printf("Tempo de execucao do INSERCAO: %lld microssegundos\n\n", vet_info[3]);
-    salvar_resultado("Insercao", vet_info, tam_vetor);
-
-    vetorCopia = copiaVet(vetor, tam_vetor);
-    reseta_vet_info(vet_info);
-    printf("ORDENACAO POR MERGE SORT:\n");
-    mede_tempo_execucao_recursivo(mergeSort, vetorCopia, 0, tam_vetor - 1, vet_info);
-    imprimeVet(vetorCopia, tam_vetor);
-    printf("MERGE SORT realizou %lld trocas.\n", vet_info[0]);
-    printf("MERGE SORT realizou %lld comparacoes.\n", vet_info[1]);
-    printf("MERGE SORT realizou %lld chamadas de funcao.\n", vet_info[2]);
-    printf("Tempo de execucao do MERGE SORT: %lld microssegundos\n\n", vet_info[3]);
-    salvar_resultado("Merge Sort", vet_info, tam_vetor);
-
-    vetorCopia = copiaVet(vetor, tam_vetor);
-    reseta_vet_info(vet_info);
-    printf("ORDENACAO POR QUICK SORT:\n");
-    mede_tempo_execucao_recursivo(quickSort, vetorCopia, 0, tam_vetor - 1, vet_info);
-    imprimeVet(vetorCopia, tam_vetor);
-    printf("QUICK SORT realizou %lld trocas.\n", vet_info[0]);
-    printf("QUICK SORT realizou %lld comparacoes.\n", vet_info[1]);
-    printf("QUICK SORT realizou %lld chamadas de funcao.\n", vet_info[2]);
-    printf("Tempo de execucao do QUICK SORT: %lld microssegundos\n\n", vet_info[3]);
-    salvar_resultado("Quick Sort", vet_info, tam_vetor);
+    //printf("\nVetor criado: \n");
+    //imprimeVet(vetor, tam_vetor);
     
-    vetorCopia = copiaVet(vetor, tam_vetor);
-    reseta_vet_info(vet_info);
-    printf("ORDENACAO POR BLOCK SORT:\n");
-    mede_tempo_execucao_recursivo(blockSort, vetorCopia, 0, tam_vetor - 1, vet_info);
-    imprimeVet(vetorCopia, tam_vetor);
-    printf("BLOCK SORT realizou %lld trocas.\n", vet_info[0]);
-    printf("BLOCK SORT realizou %lld comparacoes.\n", vet_info[1]);
-    printf("BLOCK SORT realizou %lld chamadas de funcao.\n", vet_info[2]);
-    printf("Tempo de execucao do BLOCK SORT: %lld microssegundos\n\n", vet_info[3]);
-    salvar_resultado("Block Sort", vet_info, tam_vetor);
+    if(tam_vetor < 500000){
+        vetorCopia = copiaVet(vetor, tam_vetor);
+        printf("ORDENACAO POR BOLHA:\n");
+        mede_tempo_execucao_quadratico(bolha, vetorCopia, tam_vetor, vet_info);
+        //imprimeVet(vetorCopia, tam_vetor);
+        printf("BOLHA realizou %lld trocas.\n", vet_info[0]);
+        printf("BOLHA realizou %lld comparacoes.\n", vet_info[1]);
+        printf("BOLHA realizou %lld chamadas de funcao.\n", vet_info[2]);
+        printf("Tempo de execucao do BOLHA: %lld microssegundos\n\n", vet_info[3]);
+        salvar_resultado("Bolha", vet_info, tam_vetor);
+
+        vetorCopia = copiaVet(vetor, tam_vetor);
+        reseta_vet_info(vet_info);
+        printf("ORDENACAO POR BOLHA INTELIGENTE:\n");
+        mede_tempo_execucao_quadratico(bolhaInteligente, vetorCopia, tam_vetor, vet_info);
+        //imprimeVet(vetorCopia, tam_vetor);
+        printf("BOLHA INTELIGENTE realizou %lld trocas.\n", vet_info[0]);
+        printf("BOLHA INTELIGENTE realizou %lld comparacoes.\n", vet_info[1]);
+        printf("BOLHA INTELIGENTE realizou %lld chamadas de funcao.\n", vet_info[2]);
+        printf("Tempo de execucao do BOLHA INTELIGENTE: %lld microssegundos\n\n", vet_info[3]);
+        salvar_resultado("Bolha Inteligente", vet_info, tam_vetor);
+
+        vetorCopia = copiaVet(vetor, tam_vetor);
+        reseta_vet_info(vet_info);
+        printf("ORDENACAO POR SELECAO:\n");
+        mede_tempo_execucao_quadratico(selecao, vetorCopia, tam_vetor, vet_info);
+        //imprimeVet(vetorCopia, tam_vetor);
+        printf("SELECAO realizou %lld trocas.\n", vet_info[0]);
+        printf("SELECAO realizou %lld comparacoes.\n", vet_info[1]);
+        printf("SELECAO realizou %lld chamadas de funcao.\n", vet_info[2]);
+        printf("Tempo de execucao do SELEÇÃO: %lld microssegundos\n\n", vet_info[3]);
+        salvar_resultado("Selecao", vet_info, tam_vetor);
+
+        vetorCopia = copiaVet(vetor, tam_vetor);
+        reseta_vet_info(vet_info);
+        printf("ORDENACAO POR INSERCAO:\n");
+        mede_tempo_execucao_quadratico(insercao, vetorCopia, tam_vetor, vet_info);
+        //imprimeVet(vetorCopia, tam_vetor);
+        printf("INSERCAO realizou %lld movimentacoes.\n", vet_info[0]);
+        printf("INSERCAO realizou %lld comparacoes.\n", vet_info[1]);
+        printf("INSERCAO realizou %lld chamadas de funcao.\n", vet_info[2]);
+        printf("Tempo de execucao do INSERCAO: %lld microssegundos\n\n", vet_info[3]);
+        salvar_resultado("Insercao", vet_info, tam_vetor);
+
+        vetorCopia = copiaVet(vetor, tam_vetor);
+        reseta_vet_info(vet_info);
+        printf("ORDENACAO POR MERGE SORT:\n");
+        mede_tempo_execucao_recursivo(mergeSort, vetorCopia, 0, tam_vetor - 1, vet_info);
+        //imprimeVet(vetorCopia, tam_vetor);
+        printf("MERGE SORT realizou %lld trocas.\n", vet_info[0]);
+        printf("MERGE SORT realizou %lld comparacoes.\n", vet_info[1]);
+        printf("MERGE SORT realizou %lld chamadas de funcao.\n", vet_info[2]);
+        printf("Tempo de execucao do MERGE SORT: %lld microssegundos\n\n", vet_info[3]);
+        salvar_resultado("Merge Sort", vet_info, tam_vetor);
+
+        vetorCopia = copiaVet(vetor, tam_vetor);
+        reseta_vet_info(vet_info);
+        printf("ORDENACAO POR QUICK SORT:\n");
+        mede_tempo_execucao_recursivo(quickSort, vetorCopia, 0, tam_vetor - 1, vet_info);
+        //imprimeVet(vetorCopia, tam_vetor);
+        printf("QUICK SORT realizou %lld trocas.\n", vet_info[0]);
+        printf("QUICK SORT realizou %lld comparacoes.\n", vet_info[1]);
+        printf("QUICK SORT realizou %lld chamadas de funcao.\n", vet_info[2]);
+        printf("Tempo de execucao do QUICK SORT: %lld microssegundos\n\n", vet_info[3]);
+        salvar_resultado("Quick Sort", vet_info, tam_vetor);
+    
+        vetorCopia = copiaVet(vetor, tam_vetor);
+        reseta_vet_info(vet_info);
+        printf("ORDENACAO POR BLOCK SORT:\n");
+        mede_tempo_execucao_recursivo(blockSort, vetorCopia, 0, tam_vetor - 1, vet_info);
+        //imprimeVet(vetorCopia, tam_vetor);
+        printf("BLOCK SORT realizou %lld trocas.\n", vet_info[0]);
+        printf("BLOCK SORT realizou %lld comparacoes.\n", vet_info[1]);
+        printf("BLOCK SORT realizou %lld chamadas de funcao.\n", vet_info[2]);
+        printf("Tempo de execucao do BLOCK SORT: %lld microssegundos\n\n", vet_info[3]);
+        salvar_resultado("Block Sort", vet_info, tam_vetor);
+        
+    }else{
+        vetorCopia = copiaVet(vetor, tam_vetor);
+        printf("ORDENACAO POR MERGE SORT:\n");
+        mede_tempo_execucao_recursivo(mergeSort, vetorCopia, 0, tam_vetor - 1, vet_info);
+        //imprimeVet(vetorCopia, tam_vetor);
+        printf("MERGE SORT realizou %lld trocas.\n", vet_info[0]);
+        printf("MERGE SORT realizou %lld comparacoes.\n", vet_info[1]);
+        printf("MERGE SORT realizou %lld chamadas de funcao.\n", vet_info[2]);
+        printf("Tempo de execucao do MERGE SORT: %lld microssegundos\n\n", vet_info[3]);
+        salvar_resultado("Merge Sort", vet_info, tam_vetor);
+
+        vetorCopia = copiaVet(vetor, tam_vetor);
+        reseta_vet_info(vet_info);
+        printf("ORDENACAO POR QUICK SORT:\n");
+        mede_tempo_execucao_recursivo(quickSort, vetorCopia, 0, tam_vetor - 1, vet_info);
+        //imprimeVet(vetorCopia, tam_vetor);
+        printf("QUICK SORT realizou %lld trocas.\n", vet_info[0]);
+        printf("QUICK SORT realizou %lld comparacoes.\n", vet_info[1]);
+        printf("QUICK SORT realizou %lld chamadas de funcao.\n", vet_info[2]);
+        printf("Tempo de execucao do QUICK SORT: %lld microssegundos\n\n", vet_info[3]);
+        salvar_resultado("Quick Sort", vet_info, tam_vetor);
+    
+        vetorCopia = copiaVet(vetor, tam_vetor);
+        reseta_vet_info(vet_info);
+        printf("ORDENACAO POR BLOCK SORT:\n");
+        mede_tempo_execucao_recursivo(blockSort, vetorCopia, 0, tam_vetor - 1, vet_info);
+        //imprimeVet(vetorCopia, tam_vetor);
+        printf("BLOCK SORT realizou %lld trocas.\n", vet_info[0]);
+        printf("BLOCK SORT realizou %lld comparacoes.\n", vet_info[1]);
+        printf("BLOCK SORT realizou %lld chamadas de funcao.\n", vet_info[2]);
+        printf("Tempo de execucao do BLOCK SORT: %lld microssegundos\n\n", vet_info[3]);
+        salvar_resultado("Block Sort", vet_info, tam_vetor);
+    }
 
     return 0;
 }
